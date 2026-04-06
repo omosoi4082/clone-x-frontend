fc9e7b4650491685b9221825c312614fd6926397
omosoi4082 <omosoi4082@gmail.com>
2026-04-06 17:18:34 +0900

    closes #8 init_data

---
+++ src/components/FeedItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="feed-container">
+    <div class="feed-header">
+      <div class="feed-content">{{ item.content }}</div>
+      <button class="feed-delete-button">X</button>
+    </div>
+    <div class="feed-name">{{ item.user.name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeedItem",
+  props: ["item"],
+};
+</script>
+
+<style scoped>
+.feed-container {
+  height: 80px;
+  background-color: white;
+  margin: 10px 0px;
+  color: black;
+  padding: 3%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 10px;
+}
+.feed-header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.feed-content {
+  padding: 1%;
+}
+
+.feed-delete-button {
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+.feed-name {
+  text-align: right;
+  font-size: 12px;
+}
+</style>
--- src/components/FeedList.vue
+++ src/components/FeedList.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="feed-list-container">
-    <div class="feed-container" v-for="i in 10" :key="i">
-      <div class="feed-header">
-        <div class="feed-content">{{ i }}</div>
-        <button class="feed-delete-button">X</button>
-      </div>
-      <div class="feed-name">유저</div>
-    </div>
+    <FeedItem v-for="item in items" :key="item.id" :item="item" />
   </div>
 </template>
 
 <script>
+import FeedItem from "./FeedItem.vue";
+import testData from "./TestData";
 export default {
   name: "FeedList",
+  components: { FeedItem },
+  data() {
+    return {
+      items: [],
+    };
+  },
+  created() {
+    this.items = testData;
+  },
 };
 </script>
 
@@ -20,38 +25,5 @@ export default {
 .feed-list-container {
   height: 60vh;
   overflow-y: auto;
-}
-.feed-container {
-  height: 80px;
-  background-color: white;
-  margin: 10px 0px;
-  color: black;
-  padding: 3%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  border-radius: 10px;
-}
-
-.feed-header {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.feed-content {
-  padding: 1%;
-}
-
-.feed-delete-button {
-  background: none;
-  border: none;
-  cursor: pointer;
-}
-
-.feed-name {
-  text-align: right;
-  font-size: 12px;
 }
 </style>
